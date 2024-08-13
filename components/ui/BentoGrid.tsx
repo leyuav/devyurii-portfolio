fc14c92875b5b5853e4d68e411359055b6fab84d
leyuav <leyu.av@gmail.com>
2024-08-13 09:15:23 +0200

    update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -21,7 +21,7 @@ export const BentoGrid = ({
     <div
       className={cn(
         'grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto',
-        className
+        className,
       )}
     >
       {children}
@@ -69,7 +69,7 @@ export const BentoGridItem = ({
     <div
       className={cn(
         'row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]',
-        className
+        className,
       )}
       style={{
         background: 'rgb(4,7,29)',
@@ -77,7 +77,7 @@ export const BentoGridItem = ({
       }}
     >
       <div className={`${id === 6 && 'flex justify-center'} h-full`}>
-        <div className="w-full h-full absolute">
+        <div className='w-full h-full absolute'>
           {img && (
             <Image
               src={img}
@@ -107,35 +107,35 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10',
           )}
         >
-          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+          <div className='font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10'>
             {description}
           </div>
 
-          <div className="text-lg font-sans font-bold lg:text-3xl max-w-96 z-10">{title}</div>
+          <div className='text-lg font-sans font-bold lg:text-3xl max-w-96 z-10'>{title}</div>
 
           {id === 2 && <GlobeDemo />}
           {id === 3 && (
-            <div className="flex gap-3 lg:gap-6 w-fit absolute top-2 right-5 lg:right-6">
-              <div className="flex flex-col gap-3 lg:gap-5">
+            <div className='flex gap-3 lg:gap-6 w-fit absolute top-2 right-5 lg:right-6'>
+              <div className='flex flex-col gap-3 lg:gap-5'>
                 {['React.js', 'Next.js', 'Tailwind'].map((item) => (
                   <span
                     key={item}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]'
                   >
                     {item}
                   </span>
                 ))}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+                <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]' />
               </div>
-              <div className="flex flex-col gap-3 lg:gap-5">
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
-                {['Bitwig', '', ''].map((itemSecond) => (
+              <div className='flex flex-col gap-3 lg:gap-5'>
+                <span className='py-4 px-3 rounded-lg text-center bg-[#10132e]' />
+                {['HTML/CSS', '', ''].map((itemSecond) => (
                   <span
                     key={itemSecond}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    className='py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]'
                   >
                     {itemSecond}
                   </span>
@@ -144,8 +144,8 @@ export const BentoGridItem = ({
             </div>
           )}
           {id === 6 && (
-            <div className="flex justify-center">
-              <div className="mt-5 relative">
+            <div className='flex justify-center'>
+              <div className='mt-5 relative'>
                 <div className={`absolute -bottom-5 right-0`}>
                   <Lottie
                     key={animationKey}
@@ -162,8 +162,8 @@ export const BentoGridItem = ({
                 <MagicButton
                   title={copied ? 'Email copied' : 'Copy my email'}
                   icon={<IoCopyOutline />}
-                  position="left"
-                  otherClasses="`bg-[#161a31]`"
+                  position='left'
+                  otherClasses='`bg-[#161a31]`'
                   handleClick={handleCopy}
                 />
               </div>
